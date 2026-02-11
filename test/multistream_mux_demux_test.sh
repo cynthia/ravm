716bbd3e2a8321eaf1b107bb589f8c8bc2de4b50
Marco Paniconi <marpan@google.com>
2026-02-10 22:27:30 -0800

    Re-enable _mux_demux_ test disabled in previous commit
    The avmenc test in multistream_mux_demux_test.sh
    was disabled by mistake in the previous commit:
    b977c95c73

--- test/multistream_mux_demux_test.sh
+++ test/multistream_mux_demux_test.sh
@@ -271,15 +271,15 @@ run_encode_mux_demux() {
 
   echo "Start single layer stream"
 
-  #encode_bitstream_0 || return 1
-  #encode_bitstream_1 || return 1
-  #decode_bitstream_0 || return 1
-  #decode_bitstream_1 || return 1
-  #mux_bitstreams || return 1
-  #demux_bitstream || return 1
-  #compare_bitstreams || return 1
-  #decode_muxed_bitstream || return 1
-  #compare_md5 || return 1
+  encode_bitstream_0 || return 1
+  encode_bitstream_1 || return 1
+  decode_bitstream_0 || return 1
+  decode_bitstream_1 || return 1
+  mux_bitstreams || return 1
+  demux_bitstream || return 1
+  compare_bitstreams || return 1
+  decode_muxed_bitstream || return 1
+  compare_md5 || return 1
 
   echo "Done avmenc single layer stream"
 

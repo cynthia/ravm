e9cfac2a5de29532a2522c3fad50831afed733b6
Urvang Joshi <urvang@google.com>
2026-02-20 09:19:26 -0800

    vmaf.[ch]: Revert unintentional licence text changes
    This were unintentionally changed in commit:
    c71f5643cf69146c090c8e8dbdaae36ebab0469e
    
    Revert to the prior text.

--- avm_dsp/vmaf.h
+++ avm_dsp/vmaf.h
@@ -4,10 +4,10 @@
  * This source code is subject to the terms of the BSD 3-Clause Clear License
  * and the Alliance for Open Media Patent License 1.0. If the BSD 3-Clause Clear
  * License was not distributed with this source code in the LICENSE file, you
- * can obtain it at avmedia.org/license/software-license/bsd-3-c-c/.  If the
+ * can obtain it at aomedia.org/license/software-license/bsd-3-c-c/.  If the
  * Alliance for Open Media Patent License 1.0 was not distributed with this
  * source code in the PATENTS file, you can obtain it at
- * avmedia.org/license/patent-license/.
+ * aomedia.org/license/patent-license/.
  */
 
 #ifndef AVM_AVM_DSP_VMAF_H_

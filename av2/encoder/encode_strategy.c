b314100868d18828cd5211fa695f7b1e6da9aa44
Kiran Misra <misrakir@amazon.com>
2026-03-18 18:58:12 +0000

    Encoder allows retaining long term references for S-Frames
    At encoder, reverting commit 0468755528740cff322bcd967a5221bfbc8a8525 and allowing S-frames to retain long term references.
    
    The actual underlying issue that needed to be addressed by 0468755528740cff322bcd967a5221bfbc8a8525 was that for forward key frames (OLK) encoder kept leading picture. But at the decoder (due to default usage of short_refresh_frame_flags) the leading pictures were being cleared.
    
    Fortunately commit a05a75ca64096b5d50951e31e84e2940b316fe5b fixed the actual problem by clearing the leading pictures at encoder.
    
    All `*Forward*:*ForcedKeyTestLarge*` unit tests pass locally for the MR.

--- av2/encoder/encode_strategy.c
+++ av2/encoder/encode_strategy.c
@@ -762,13 +762,8 @@ int av2_get_refresh_frame_flags(
     return (1 << cpi->common.seq_params.ref_frames) - 1;
   }
 
-  // S-Frames (non-RAS) overwrite all reference slots
-  if (frame_params->frame_type == S_FRAME && cpi->is_ras_frame != 1) {
-    return (1 << cpi->common.seq_params.ref_frames) - 1;
-  }
-
-  if (cpi->is_ras_frame == 1 && (frame_params->frame_type == S_FRAME ||
-                                 frame_params->frame_type == KEY_FRAME)) {
+  if (frame_params->frame_type == S_FRAME ||
+      frame_params->frame_type == KEY_FRAME) {
     AV2_COMMON *const cm = &cpi->common;
     int refresh_frame_flags = (1 << cpi->common.seq_params.ref_frames) - 1;
     cm->num_ref_key_frames = 0;
